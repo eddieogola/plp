0b2656f20cf70ccc846ea5b94b95b3f4e742998a
eddieogola <39475602+eddieogola@users.noreply.github.com>
2022-10-02 14:42:02 +0300

    Update static_values.dart

--- frontend/lib/common/static_values.dart
+++ frontend/lib/common/static_values.dart
@@ -2,7 +2,7 @@ class StaticValues {
   const StaticValues._();
 
   // Connection
-  static const String apiUrl = "http://192.168.2.106:8000/";
+  static const String apiUrl = "http://YOUR_IP_ADDRESS:8000/";
 
   //Storage
   static const String SELLER_DATA = 'sellerData';
